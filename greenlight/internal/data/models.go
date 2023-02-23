33eb289063a207f72c5793176e3698733f046fb0
Alisher128 <106400930+Alisher128@users.noreply.github.com>
2023-02-23 11:15:01 +0600

    Add files via upload

---
+++ greenlight/internal/data/models.go
@@ -0,0 +1,29 @@
+package data
+
+import (
+	"database/sql"
+	"errors"
+)
+
+var (
+	ErrRecordNotFound = errors.New("record not found")
+	ErrEditConflict   = errors.New("edit conflict")
+)
+
+// Create a Models struct which wraps the MovieModel. We'll add other models to this,
+// like a UserModel and PermissionModel, as our build progresses.
+type Models struct {
+	Movies      MovieModel
+	Permissions PermissionModel // Add a new Permissions field.
+	Tokens      TokenModel
+	Users       UserModel
+}
+
+func NewModels(db *sql.DB) Models {
+	return Models{
+		Movies:      MovieModel{DB: db},
+		Permissions: PermissionModel{DB: db}, // Initialize a new PermissionModel instance.
+		Tokens:      TokenModel{DB: db},
+		Users:       UserModel{DB: db},
+	}
+}
